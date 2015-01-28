2695001b36f17dcf8f0adb30fd4ffdc5adf20cb8
ststeiger <quandary82@hailmail.net>
2015-01-28 14:34:09 +0100

    Dragout

--- DnDFileUpload/Properties/AssemblyInfo.cs
+++ DnDFileUpload/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2014.12.2.1400")]
-[assembly: AssemblyFileVersion("2014.12.2.1400")]
+[assembly: AssemblyVersion("2015.1.28.1427")]
+[assembly: AssemblyFileVersion("2015.1.28.1427")]
---
+++ DnDFileUpload/ajax/GetFile.ashx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace DnDFileUpload.ajax
+{
+    /// <summary>
+    /// Zusammenfassungsbeschreibung für GetFile
+    /// </summary>
+    public class GetFile : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string name = context.Request.Params["name"];
+            string fn = context.Server.MapPath("~/downloads/" + name);
+            string FileName = System.IO.Path.GetFileName(fn);
+            System.IO.FileInfo fi = new System.IO.FileInfo(fn);
+
+            context.Response.ClearHeaders();
+            context.Response.ClearContent();
+            context.Response.Clear();
+            context.Response.AddHeader("Content-Disposition", "attachment; filename=" + FileName);
+            context.Response.AddHeader("Content-Length", fi.Length.ToString());
+            context.Response.ContentType = "application/octet-stream";
+            context.Response.TransmitFile(fn);
+            context.Response.End();
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
