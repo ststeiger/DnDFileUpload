78bb35ee0bc6881e0d181f2ae717b1d21b7ed8b7
ststeiger <quandary82@hailmail.net>
2019-08-02 17:31:06 +0200

    pf

--- DnDFileUpload/SQL/query/[T_VWS_Ref_PdfBibliotheksGruppe].sql
+++ DnDFileUpload/SQL/query/[T_VWS_Ref_PdfBibliotheksGruppe].sql
@@ -1,9 +1,5 @@
 
-INSERT [dbo].[T_VWS_Ref_PdfBibliotheksGruppe] ([PBG_UID], [PBG_PBG_UID], [PBG_Name], [PBG_Sort], [PBG_Status]) VALUES (N'db01ae61-7e3a-4f8c-86cf-4df7fb6f733b', NULL, N'Zeichnungselemente', 0, 1)
-GO
-INSERT [dbo].[T_VWS_Ref_PdfBibliotheksGruppe] ([PBG_UID], [PBG_PBG_UID], [PBG_Name], [PBG_Sort], [PBG_Status]) VALUES (N'01bf9d21-e3e8-432a-87ff-9d943ec0f8c3', NULL, N'Fluchtplan', 0, 1)
-GO
-INSERT [dbo].[T_VWS_Ref_PdfBibliotheksGruppe] ([PBG_UID], [PBG_PBG_UID], [PBG_Name], [PBG_Sort], [PBG_Status]) VALUES (N'6737f36c-147e-4f78-a2d2-9fe0ef04e111', NULL, N'Logos', 0, 1)
-GO
-INSERT [dbo].[T_VWS_Ref_PdfBibliotheksGruppe] ([PBG_UID], [PBG_PBG_UID], [PBG_Name], [PBG_Sort], [PBG_Status]) VALUES (N'1e306062-9529-4bfd-b1bd-b4b5e5103edc', NULL, N'Übersichtszeichnungen', 0, 1)
-GO
+INSERT INTO [T_VWS_Ref_PdfBibliotheksGruppe] ([PBG_UID], [PBG_PBG_UID], [PBG_Name], [PBG_Sort], [PBG_Status]) VALUES (N'db01ae61-7e3a-4f8c-86cf-4df7fb6f733b', NULL, N'Zeichnungselemente', 0, 1);
+INSERT INTO [T_VWS_Ref_PdfBibliotheksGruppe] ([PBG_UID], [PBG_PBG_UID], [PBG_Name], [PBG_Sort], [PBG_Status]) VALUES (N'01bf9d21-e3e8-432a-87ff-9d943ec0f8c3', NULL, N'Fluchtplan', 0, 1);
+INSERT INTO [T_VWS_Ref_PdfBibliotheksGruppe] ([PBG_UID], [PBG_PBG_UID], [PBG_Name], [PBG_Sort], [PBG_Status]) VALUES (N'6737f36c-147e-4f78-a2d2-9fe0ef04e111', NULL, N'Logos', 0, 1);
+INSERT INTO [T_VWS_Ref_PdfBibliotheksGruppe] ([PBG_UID], [PBG_PBG_UID], [PBG_Name], [PBG_Sort], [PBG_Status]) VALUES (N'1e306062-9529-4bfd-b1bd-b4b5e5103edc', NULL, N'Übersichtszeichnungen', 0, 1);
